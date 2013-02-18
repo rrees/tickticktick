2a79640c6c2b5604e896d098f652d07cd1bcd400
Robert Rees <robert.rees@guardian.co.uk>
2013-02-18 11:04:18 +0000

    Creates a working Knockout binding and the api endpoint for Knockout to consume

---
+++ api.py
@@ -0,0 +1,36 @@
+import webapp2
+import os
+import json
+import logging
+from urllib import quote, urlencode
+from google.appengine.api import urlfetch
+from google.appengine.api import memcache
+
+import data_filters
+import extract
+import headers
+
+class Content(webapp2.RequestHandler):
+	def get(self, minimum, maximum):
+
+		logging.info(maximum)
+
+		result = {}
+
+		data = memcache.get("today")
+
+
+		if data:
+			all_content = json.loads(data)
+			valid_filter = lambda x: data_filters.has_wordcount(x) and data_filters.minutes(int(minimum), int(maximum))(x) and data_filters.valid_section(x)
+			appropriate_content = filter(valid_filter, all_content)
+			sorted_content = sorted(appropriate_content, key = lambda x: int(extract.wordcount(x)), reverse = True)
+
+			logging.info([d["fields"]["wordcount"] for d in sorted_content])
+			result['content'] = sorted_content
+
+		headers.json(self.response)
+		self.response.out.write(json.dumps(result))
+
+app = webapp2.WSGIApplication([('/api/content/from/(\d+)/to/(\d+)', Content)],
+                              debug=True)
